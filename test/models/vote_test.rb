21d4de193277bf4b4bb015a21581f8c77ab9ed0e
Molly G <gehring.mg@gmail.com>
2016-02-23 14:25:24 -0500

    Test vote can be cast

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -4,4 +4,12 @@ class VoteTest < ActiveSupport::TestCase
   def test_class_exists
     assert Vote
   end
+
+  def test_new_vote_can_be_created
+    c = Candidate.create!(id: 1, name: "The Donald")
+    v = Voter.create!(id: 3, name: "George Washington")
+    vote = Vote.create!(candidate_id: c.id, voter_id: v.id)
+    assert_equal 1, vote.candidate_id
+    assert_equal 3, vote.voter_id
+  end
 end
