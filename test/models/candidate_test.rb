d9a8808e9ac5b8f1bab745954770e978d7cfe164
Molly G <gehring.mg@gmail.com>
2016-02-23 14:20:10 -0500

    Test models can be created

--- test/models/candidate_test.rb
+++ test/models/candidate_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CandidateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_class_exists
+    assert Candidate
+  end
+
+  def test_new_candidate_can_be_created
+    c = Candidate.create!(name: "The Donald")
+    assert_equal "The Donald", c.name
+  end
 end
--- test/models/voter_test.rb
+++ test/models/voter_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class VoterTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_class_exists
+    assert Voter
+  end
+
+  def test_new_voter_can_be_created
+    v = Voter.create!(name: "George Washington")
+    assert_equal "George Washington", v.name
+  end
 end
